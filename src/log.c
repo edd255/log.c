30916e6928b0b5540bdb1cebf093f4c44bc82075
edd255 <abbadon@disroot.org>
2023-08-12 20:45:37 +0200

    refactor: made variable names clearer

--- src/log.c
+++ src/log.c
@@ -25,80 +25,80 @@
 #define MAX_CALLBACKS 32
 
 typedef struct {
-    log_LogFn fn;
+    logc_logfn_t fn;
     void* udata;
     int level;
-} Callback;
+} callback_t;
 
 static struct {
     void* udata;
-    log_LockFn lock;
+    logc_lockfn_t lock;
     int level;
     bool quiet;
-    Callback callbacks[MAX_CALLBACKS];
-} L;
+    callback_t callbacks[MAX_CALLBACKS];
+} logc_logger_t;
 
 static const char* level_strings[] =
     {"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
 
-#ifdef LOG_USE_COLOR
+#ifdef LOGC_USE_COLOR
 static const char* level_colors[] =
     {"\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"};
 #endif
 
-static void stdout_callback(log_Event* ev) {
+static void stdout_callback(log_event_t* event) {
     char buf[16];
-    buf[strftime(buf, sizeof(buf), "%H:%M:%S", ev->time)] = '\0';
-#ifdef LOG_USE_COLOR
+    buf[strftime(buf, sizeof(buf), "%H:%M:%S", event->time)] = '\0';
+#ifdef LOGC_USE_COLOR
     fprintf(
-        ev->udata,
-        "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
+        event->udata,
+        "%s \x1b[1m%s%-5s\x1b[0m %s:%d - ",
         buf,
-        level_colors[ev->level],
-        level_strings[ev->level],
-        ev->file,
-        ev->line
+        level_colors[event->level],
+        level_strings[event->level],
+        event->file,
+        event->line,
     );
 #else
     fprintf(
-        ev->udata,
-        "%s %-5s %s:%d: ",
+        event->udata,
+        "%s %-5s %s:%d -",
         buf,
-        level_strings[ev->level],
-        ev->file,
-        ev->line
+        level_strings[event->level],
+        event->file,
+        event->line
     );
 #endif
-    vfprintf(ev->udata, ev->fmt, ev->ap);
-    fprintf(ev->udata, "\n");
-    fflush(ev->udata);
+    vfprintf(event->udata, event->fmt, event->args);
+    fprintf(event->udata, "\n");
+    fflush(event->udata);
 }
 
-static void file_callback(log_Event* ev) {
+static void file_callback(log_event_t* event) {
     char buf[64];
-    buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", ev->time)] = '\0';
+    buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", event->time)] = '\0';
     fprintf(
-        ev->udata,
-        "%s %-5s %s:%d: ",
+        event->udata,
+        "%s %-5s %s:%d - ",
         buf,
-        level_strings[ev->level],
-        ev->file,
-        ev->line
+        level_strings[event->level],
+        event->file,
+        event->line
     );
-    vfprintf(ev->udata, ev->fmt, ev->ap);
-    fprintf(ev->udata, "\n");
-    fflush(ev->udata);
+    vfprintf(event->udata, event->fmt, event->args);
+    fprintf(event->udata, "\n");
+    fflush(event->udata);
 }
 
 static void lock(void) {
-    if (L.lock) {
-        L.lock(true, L.udata);
+    if (logc_logger_t.lock) {
+        logc_logger_t.lock(true, logc_logger_t.udata);
     }
 }
 
 static void unlock(void) {
-    if (L.lock) {
-        L.lock(false, L.udata);
+    if (logc_logger_t.lock) {
+        logc_logger_t.lock(false, logc_logger_t.udata);
     }
 }
 
@@ -106,23 +106,23 @@ const char* log_level_string(int level) {
     return level_strings[level];
 }
 
-void log_set_lock(log_LockFn fn, void* udata) {
-    L.lock = fn;
-    L.udata = udata;
+void log_set_lock(logc_lockfn_t fn, void* udata) {
+    logc_logger_t.lock = fn;
+    logc_logger_t.udata = udata;
 }
 
 void log_set_level(int level) {
-    L.level = level;
+    logc_logger_t.level = level;
 }
 
 void log_set_quiet(bool enable) {
-    L.quiet = enable;
+    logc_logger_t.quiet = enable;
 }
 
-int log_add_callback(log_LogFn fn, void* udata, int level) {
+int log_add_callback(logc_logfn_t fn, void* udata, int level) {
     for (int i = 0; i < MAX_CALLBACKS; i++) {
-        if (!L.callbacks[i].fn) {
-            L.callbacks[i] = (Callback) {fn, udata, level};
+        if (!logc_logger_t.callbacks[i].fn) {
+            logc_logger_t.callbacks[i] = (callback_t) {fn, udata, level};
             return 0;
         }
     }
@@ -133,40 +133,36 @@ int log_add_fp(FILE* fp, int level) {
     return log_add_callback(file_callback, fp, level);
 }
 
-static void init_event(log_Event* ev, void* udata) {
-    if (!ev->time) {
+static void init_event(log_event_t* event, void* udata) {
+    if (!event->time) {
         time_t t = time(NULL);
-        ev->time = localtime(&t);
+        event->time = localtime(&t);
     }
-    ev->udata = udata;
+    event->udata = udata;
 }
 
 void log_log(int level, const char* file, int line, const char* fmt, ...) {
-    log_Event ev = {
+    log_event_t event = {
         .fmt = fmt,
         .file = file,
         .line = line,
         .level = level,
     };
-
     lock();
-
-    if (!L.quiet && level >= L.level) {
-        init_event(&ev, stderr);
-        va_start(ev.ap, fmt);
-        stdout_callback(&ev);
-        va_end(ev.ap);
+    if (!logc_logger_t.quiet && level >= logc_logger_t.level) {
+        init_event(&event, stderr);
+        va_start(event.args, fmt);
+        stdout_callback(&event);
+        va_end(event.args);
     }
-
-    for (int i = 0; i < MAX_CALLBACKS && L.callbacks[i].fn; i++) {
-        Callback* cb = &L.callbacks[i];
+    for (int i = 0; i < MAX_CALLBACKS && logc_logger_t.callbacks[i].fn; i++) {
+        callback_t* cb = &logc_logger_t.callbacks[i];
         if (level >= cb->level) {
-            init_event(&ev, cb->udata);
-            va_start(ev.ap, fmt);
-            cb->fn(&ev);
-            va_end(ev.ap);
+            init_event(&event, cb->udata);
+            va_start(event.args, fmt);
+            cb->fn(&event);
+            va_end(event.args);
         }
     }
-
     unlock();
 }
--- src/log.h
+++ src/log.h
@@ -13,22 +13,22 @@
 #include <stdio.h>
 #include <time.h>
 
-#define LOG_VERSION "0.1.0"
+#define LOG_VERSION "0.2.0"
 
 typedef struct {
-    va_list ap;
+    va_list args;
     const char* fmt;
     const char* file;
     struct tm* time;
     void* udata;
     int line;
     int level;
-} log_Event;
+} log_event_t;
 
-typedef void (*log_LogFn)(log_Event* ev);
-typedef void (*log_LockFn)(bool lock, void* udata);
+typedef void (*logc_logfn_t)(log_event_t* ev);
+typedef void (*logc_lockfn_t)(bool lock, void* udata);
 
-enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
+enum { LOGC_TRACE, LOGC_DEBUG, LOGC_INFO, LOGC_WARN, LOGC_ERROR, LOGC_FATAL };
 
 #define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
 #define log_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
@@ -38,10 +38,10 @@ enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 #define log_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
 const char* log_level_string(int level);
-void log_set_lock(log_LockFn fn, void* udata);
+void log_set_lock(logc_lockfn_t fn, void* udata);
 void log_set_level(int level);
 void log_set_quiet(bool enable);
-int log_add_callback(log_LogFn fn, void* udata, int level);
+int log_add_callback(logc_logfn_t fn, void* udata, int level);
 int log_add_fp(FILE* fp, int level);
 
 void log_log(int level, const char* file, int line, const char* fmt, ...);
